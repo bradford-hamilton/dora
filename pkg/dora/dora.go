632e856a8b975c24072f28df6240df51c5783490
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-04-12 10:42:57 -0600

    pkg/dora/{dora,parse}: No logical changes in this PR. This moves the scanning/parsing logic for queries into another file called parse. Same package - dora - for now

--- pkg/dora/dora.go
+++ pkg/dora/dora.go
@@ -4,7 +4,6 @@ package dora
 import (
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/bradford-hamilton/dora/pkg/ast"
 	"github.com/bradford-hamilton/dora/pkg/lexer"
@@ -69,14 +68,6 @@ func (c *Client) setQuery(query []rune) {
 	c.query = query
 }
 
-// queryToken represents a single "step" in each query.
-// Queries are parsed into a []queryTokens to be used for exploring the JSON.
-type queryToken struct {
-	accessType string // object or array
-	keyReq     string // a key like "name"
-	indexReq   int    // an index selection like 0, 1, 2
-}
-
 func (c *Client) parseQuery() error {
 	tokens, err := scanQueryTokens(c.query)
 	if err != nil {
@@ -86,125 +77,12 @@ func (c *Client) parseQuery() error {
 	return nil
 }
 
-// scanQueryTokens scans a users query input into a collection of queryTokens.
-// Dora's query syntax is very straight forward, here is a quick BNF-like representation:
-//	  <dora-query>  ::= <querystring>
-//    <querystring> ::= "<query>,*"
-//    <query>       ::= "[<int>]" | ".<string>"
-func scanQueryTokens(query []rune) ([]queryToken, error) {
-	var qts []queryToken
-	queryLen := len(query)
-
-	// Start at 1 to ignore the `$`, which has already been validated at this point.
-	for i := 1; i < queryLen-1; i++ {
-		switch query[i] {
-		case '.':
-			// Step into the key, ex:
-			// - If we were at the `.` in `.name` this bumps us to `n`.
-			i++
-
-			// Retrieve the selector and how far to increase `i` (jump).
-			s, jump, _, err := parseObjSelector(query[i:])
-			if err != nil {
-				return []queryToken{}, err
-			}
-
-			// Append our new query token and adjust the jump.
-			qts = append(qts, queryToken{accessType: "object", keyReq: string(s)})
-			i += jump - 1
-		case '[':
-			// Step into the index, ex:
-			// - If we were at the `[` in `[123]` this bumps us to `1`
-			i++
-
-			// Retrieve the selector and how far to increase `i` (jump).
-			s, jump, err := parseArraySelector(query[i:])
-			if err != nil {
-				return []queryToken{}, err
-			}
-
-			// The array selector is an int, and we want assert that.
-			index, err := strconv.Atoi(string(s))
-			if err != nil {
-				return []queryToken{}, err
-			}
-
-			// Append our new query token and adjust the jump
-			qts = append(qts, queryToken{accessType: "array", indexReq: index})
-			i += jump
-		default:
-			return []queryToken{}, errSelectorSytax(string(query[i]))
-		}
-	}
-
-	return qts, nil
-}
-
-func parseObjSelector(queryChunk []rune) ([]rune, int, bool, error) {
-	var jump int
-	var isIndex bool
-	queryLen := len(queryChunk)
-
-	// Consume the key name
-	if isPropertyKey(queryChunk[jump]) {
-		for isPropertyKey(queryChunk[jump]) && jump < queryLen-1 {
-			jump++
-		}
-
-		// After consuming the property key above, we should be onto the next selector.
-		// This has to either be another object or an array
-		if queryChunk[jump] == '.' || queryChunk[jump] == '[' {
-			return queryChunk[0:jump], jump, isIndex, nil
-		} else if jump == queryLen-1 {
-			// we are on the last byte
-			return queryChunk[0 : jump+1], jump, isIndex, nil
-		}
-
-		return nil, 0, isIndex, errSelectorSytax(string(queryChunk[jump]))
-	}
-
-	return nil, 0, isIndex, fmt.Errorf(
-		"Error parsing object selector within query. Expected string, but started with %s",
-		string(queryChunk[jump]),
-	)
-}
-
-func parseArraySelector(queryChunk []rune) ([]rune, int, error) {
-	var jump int
-	queryLen := len(queryChunk)
-
-	// Consume the index and return it along with the jump
-	if isNumber(queryChunk[jump]) {
-		for isNumber(queryChunk[jump]) && jump < queryLen-1 {
-			jump++
-		}
-		return queryChunk[0:jump], jump, nil
-	}
-
-	return nil, 0, fmt.Errorf(
-		"Error parsing array selector within query. Expected an int, but started with %s",
-		string(queryChunk[jump]),
-	)
-}
-
 func (c *Client) executeQuery() error {
 	// TODO: actually execute the query next
 	fmt.Println(c.query)
 	// parseQuery into parsedQuery type deal?
 	// iterate through c.program to fetch user request
 	return nil
-}
-
-func isPropertyKey(char rune) bool {
-	return isLetter(char) || isNumber(char)
-}
-
-func isLetter(char rune) bool {
-	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
-}
-
-func isNumber(char rune) bool {
-	return '0' <= char && char <= '9'
 }
 
 func validateQueryRoot(query string, rootNodeType ast.RootNodeType) error {
@@ -242,45 +120,3 @@ func errSelectorSytax(operator string) error {
 		operator,
 	)
 }
-
-/*
-	JSON Object:
-	{
-		"name": "bradford",
-		"array": ["some", "values"]
-		"obj": {
-			"innerKey": {
-				"innerKey2": "innerValue"
-			}
-		}
-	}
-
-	- Query must start with $.
-	$.name 							== "bradford"
-	$.array 						== ["array", "values"]
-	$.array[0] 						== "some"
-	$.array[1] 						== "values"
-	$.array[2] 						== error
-
-	$.obj.innerKey.innerKey2 		== "innerValue"
-	$.obj["innerKey"].innerKey2 	== "innerValue"
-	$.obj.innerKey["innerKey2"] 	== "innerValue"
-	$.obj["innerKey"]["innerKey2"] 	== "innerValue"
-
-	---
-
-	JSON Array:
-	[
-		"some",
-		"values",
-		{ "objKey": "objValue" }
-	]
-
-	- Query must start with $.
-	$[0] 			== "some"
-	$[1] 			== "values"
-	$[2] 			== { "objKey": "objValue" }
-	$[2].objKey 	== "objValue"
-	$[2]["objKey"] 	== "objValue"
-
-*/
---
+++ pkg/dora/parse.go
@@ -0,0 +1,127 @@
+package dora
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// queryToken represents a single "step" in each query.
+// Queries are parsed into a []queryTokens to be used for exploring the JSON.
+type queryToken struct {
+	accessType string // object or array
+	keyReq     string // a key like "name"
+	indexReq   int    // an index selection like 0, 1, 2
+}
+
+// scanQueryTokens scans a users query input into a collection of queryTokens.
+// Dora's query syntax is very straight forward, here is a quick BNF-like representation:
+//	  <dora-query>  ::= <querystring>
+//    <querystring> ::= "<query>,*"
+//    <query>       ::= "[<int>]" | ".<string>"
+func scanQueryTokens(query []rune) ([]queryToken, error) {
+	var qts []queryToken
+	queryLen := len(query)
+
+	// Start at 1 to ignore the `$`, which has already been validated at this point.
+	for i := 1; i < queryLen-1; i++ {
+		switch query[i] {
+		case '.':
+			// Step into the key, ex:
+			// - If we were at the `.` in `.name` this bumps us to `n`.
+			i++
+
+			// Retrieve the selector and how far to increase `i` (jump).
+			s, jump, _, err := parseObjSelector(query[i:])
+			if err != nil {
+				return []queryToken{}, err
+			}
+
+			// Append our new query token and adjust the jump.
+			qts = append(qts, queryToken{accessType: "object", keyReq: string(s)})
+			i += jump - 1
+		case '[':
+			// Step into the index, ex:
+			// - If we were at the `[` in `[123]` this bumps us to `1`
+			i++
+
+			// Retrieve the selector and how far to increase `i` (jump).
+			s, jump, err := parseArraySelector(query[i:])
+			if err != nil {
+				return []queryToken{}, err
+			}
+
+			// The array selector is an int, and we want assert that.
+			index, err := strconv.Atoi(string(s))
+			if err != nil {
+				return []queryToken{}, err
+			}
+
+			// Append our new query token and adjust the jump
+			qts = append(qts, queryToken{accessType: "array", indexReq: index})
+			i += jump
+		default:
+			return []queryToken{}, errSelectorSytax(string(query[i]))
+		}
+	}
+
+	return qts, nil
+}
+
+func parseObjSelector(queryChunk []rune) ([]rune, int, bool, error) {
+	var jump int
+	var isIndex bool
+	queryLen := len(queryChunk)
+
+	// Consume the key name
+	if isPropertyKey(queryChunk[jump]) {
+		for isPropertyKey(queryChunk[jump]) && jump < queryLen-1 {
+			jump++
+		}
+
+		// After consuming the property key above, we should be onto the next selector.
+		// This has to either be another object or an array
+		if queryChunk[jump] == '.' || queryChunk[jump] == '[' {
+			return queryChunk[0:jump], jump, isIndex, nil
+		} else if jump == queryLen-1 {
+			// we are on the last byte
+			return queryChunk[0 : jump+1], jump, isIndex, nil
+		}
+
+		return nil, 0, isIndex, errSelectorSytax(string(queryChunk[jump]))
+	}
+
+	return nil, 0, isIndex, fmt.Errorf(
+		"Error parsing object selector within query. Expected string, but started with %s",
+		string(queryChunk[jump]),
+	)
+}
+
+func parseArraySelector(queryChunk []rune) ([]rune, int, error) {
+	var jump int
+	queryLen := len(queryChunk)
+
+	// Consume the index and return it along with the jump
+	if isNumber(queryChunk[jump]) {
+		for isNumber(queryChunk[jump]) && jump < queryLen-1 {
+			jump++
+		}
+		return queryChunk[0:jump], jump, nil
+	}
+
+	return nil, 0, fmt.Errorf(
+		"Error parsing array selector within query. Expected an int, but started with %s",
+		string(queryChunk[jump]),
+	)
+}
+
+func isPropertyKey(char rune) bool {
+	return isLetter(char) || isNumber(char)
+}
+
+func isLetter(char rune) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func isNumber(char rune) bool {
+	return '0' <= char && char <= '9'
+}
